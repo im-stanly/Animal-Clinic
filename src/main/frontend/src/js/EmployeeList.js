737a1e6cb3898676fafea4b50887403776497a01
loordoza <131matmil131@gmail.com>
2023-06-11 01:27:20 +0200

    cleanup

--- src/main/frontend/src/js/EmployeeList.js
+++ src/main/frontend/src/js/EmployeeList.js
@@ -9,11 +9,9 @@ const EmployeeList = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!token) {
+    if (!token || decodeRoleFromToken(token) !== 'admin') {
       navigate("/");
-    } else if(decodeRoleFromToken(token) !== 'admin') {
-      navigate("/");
-    }else {
+    } else {
       fetchEmployees();
     }
   }, [token]);
@@ -41,17 +39,14 @@ const EmployeeList = () => {
   const [filteredEmployees, setFilteredEmployees] = useState([]);
   const [filters, setFilters] = useState({
     position: '',
-    fav_animal: ''
   });
   const [suggestedPositions, setSuggestedPositions] = useState([]);
-  const [suggestedAnimals, setSuggestedAnimals] = useState([]);
   const [newEmployee, setNewEmployee] = useState({
     person_id: '',
     position: '',
     salary: '',
     date_start: '',
     date_fire: null,
-    fav_animal: ''
   });
 
   useEffect(() => {
@@ -60,12 +55,11 @@ const EmployeeList = () => {
 
   const fetchEmployees = async () => {
     try {
-      const response = await fetch('http://localhost:8080/employees');
+      const response = await fetch('http://localhost:8080/employees/details');
       const data = await response.json();
       setEmployees(data);
       setFilteredEmployees(data);
       setSuggestedPositions(getUniqueValues(data, 'position'));
-      setSuggestedAnimals(getUniqueValues(data, 'fav_animal'));
     } catch (error) {
       console.error(error);
     }
@@ -96,42 +90,7 @@ const EmployeeList = () => {
       );
     }
 
-    if (filters.fav_animal) {
-      filtered = filtered.filter(
-        (employee) => employee.fav_animal === filters.fav_animal
-      );
-    }
-
     setFilteredEmployees(filtered);
-  };
-
-  const handleAddEmployee = async () => {
-    try {
-      const jsonData = [newEmployee];
-      console.log('Sending JSON:', JSON.stringify(jsonData));
-      const response = await fetch('http://localhost:8080/employees', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonData)
-      });
-      if (response.ok) {
-        setNewEmployee({
-          person_id: '',
-          position: '',
-          salary: '',
-          date_start: '',
-          date_fire: null,
-          fav_animal: ''
-        });
-        fetchEmployees();
-      } else {
-        console.error('Failed to add employee');
-      }
-    } catch (error) {
-      console.error(error);
-    }
   };
 
   const handleFireEmployee = async (id) => {
@@ -141,7 +100,7 @@ const EmployeeList = () => {
         date_fire: date_fire
       };
 
-      console.log('Wysyłany JSON:', JSON.stringify(data));
+      console.log('Sending JSON:', JSON.stringify(data));
 
       await fetch(`http://localhost:8080/employees/id=${id}`, {
         method: 'PATCH',
@@ -157,16 +116,46 @@ const EmployeeList = () => {
     }
   };
 
+  const handlePermissionChange = async (event) => {
+    event.preventDefault();
+
+    const personId = event.target.elements.employeeId.value;
+    const permissionLevel = event.target.elements.permissionLevel.value;
+
+    try {
+      const data = {
+        person_id: personId,
+        permissionLevel: permissionLevel
+      };
+
+      console.log('Sending JSON:', JSON.stringify(data));
+
+      await fetch(`http://localhost:8080/accounts/permission`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      fetchEmployees();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
   return (
     <div className="employee-list-container">
-      <h1>Lista Pracowników</h1>
+      <h1>Employee List</h1>
 
-        <Link to="/" className="back-link" onClick={handleLogout}>
-            Wyloguj
-        </Link>
+      <Link to="/" className="back-link" onClick={handleLogout}>
+        Logout
+      </Link>
 
       <div>
-        <label htmlFor="position">Stanowisko:</label>
+        <label htmlFor="position">Position:</label>
         <input
           type="text"
           id="position"
@@ -182,52 +171,35 @@ const EmployeeList = () => {
         </datalist>
       </div>
 
-      <div>
-        <label htmlFor="fav_animal">Ulubione zwierzę:</label>
-        <input
-          type="text"
-          id="fav_animal"
-          name="fav_animal"
-          value={filters.fav_animal}
-          onChange={handleFilterChange}
-          list="suggestedAnimals"
-        />
-        <datalist id="suggestedAnimals">
-          {suggestedAnimals.map((animal) => (
-            <option key={animal} value={animal} />
-          ))}
-        </datalist>
-      </div>
-
       <table>
         <thead>
           <tr>
             <th>ID</th>
-            <th>Osoba ID</th>
-            <th>Stanowisko</th>
-            <th>Wynagrodzenie</th>
-            <th>Data rozpoczęcia</th>
-            <th>Data zwolnienia</th>
-            <th>Ulubione zwierzę</th>
-            <th>Akcje</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Position</th>
+            <th>Salary</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {filteredEmployees.map((employee) => (
             <tr key={employee.id} className="employee-item">
               <td>{employee.id}</td>
-              <td>{employee.person_id}</td>
+              <td>{employee.first_name}</td>
+              <td>{employee.last_name}</td>
               <td>{employee.position}</td>
               <td>{employee.salary}</td>
               <td>{employee.date_start}</td>
               <td>{employee.date_fire}</td>
-              <td>{employee.fav_animal}</td>
               <td className="employee-actions">
                 <button
                   className="fire-button"
                   onClick={() => handleFireEmployee(employee.id)}
                 >
-                  Zwolnij!
+                  Fire!
                 </button>
               </td>
             </tr>
@@ -235,85 +207,27 @@ const EmployeeList = () => {
         </tbody>
       </table>
 
-      <h2>Dodaj pracownika</h2>
-      <div>
-        <label htmlFor="person_id">ID Osoby:</label>
+      <h2>Assign Permissions</h2>
+      <form className="assign-permission-form" onSubmit={handlePermissionChange}>
+        <label htmlFor="employeeId">Account ID:</label>
         <input
           type="text"
-          id="person_id"
-          name="person_id"
-          value={newEmployee.person_id}
-          onChange={(event) =>
-            setNewEmployee({
-              ...newEmployee,
-              person_id: event.target.value
-            })
-          }
+          id="employeeId"
+          name="employeeId"
+          required
         />
-      </div>
-      <div>
-        <label htmlFor="position">Stanowisko:</label>
-        <input
-          type="text"
-          id="position"
-          name="position"
-          value={newEmployee.position}
-          onChange={(event) =>
-            setNewEmployee({
-              ...newEmployee,
-              position: event.target.value
-            })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="salary">Wynagrodzenie:</label>
-        <input
-          type="text"
-          id="salary"
-          name="salary"
-          value={newEmployee.salary}
-          onChange={(event) =>
-            setNewEmployee({
-              ...newEmployee,
-              salary: event.target.value
-            })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="date_start">Data rozpoczęcia:</label>
-        <input
-          type="text"
-          id="date_start"
-          name="date_start"
-          value={newEmployee.date_start}
-          onChange={(event) =>
-            setNewEmployee({
-              ...newEmployee,
-              date_start: event.target.value
-            })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="fav_animal">Ulubione zwierzę:</label>
-        <input
-          type="text"
-          id="fav_animal"
-          name="fav_animal"
-          value={newEmployee.fav_animal}
-          onChange={(event) =>
-            setNewEmployee({
-              ...newEmployee,
-              fav_animal: event.target.value
-            })
-          }
-        />
-      </div>
-      <button className="add-button" onClick={handleAddEmployee}>
-        Dodaj pracownika
-      </button>
+        <label htmlFor="permissionLevel">Permission Level:</label>
+        <select
+          name="permissionLevel"
+          id="permissionLevel"
+          required
+        >
+          <option value="user">User</option>
+          <option value="employee">Employee</option>
+          <option value="admin">Administrator</option>
+        </select>
+        <button type="submit">Assign Permissions</button>
+      </form>
     </div>
   );
 };
